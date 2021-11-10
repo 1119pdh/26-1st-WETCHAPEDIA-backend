6d2880319d9cc45e61b73a693f1c69d87295c7c0
Minjeong Bak <doniminp@gmail.com>
2021-11-10 17:48:27 +0900

    불필요한 코드 삭제

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,6 @@
-import json, re
+import json
 
-from django.conf       import settings
-from django.http       import JsonResponse, request
+from django.http       import JsonResponse
 from django.views      import View
 from django.db.models  import Q, Avg
 
